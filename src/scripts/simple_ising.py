60596b47264d3636438600a27cc51108ad879efa
Shivansh Seth <shivansh.seth@research.iiit.ac.in>
2022-08-10 10:36:29 +0530

    simple ising model for network analysis

--- src/scripts/simple_ising.py
+++ src/scripts/simple_ising.py
@@ -4,16 +4,12 @@ from numpy.random import rand
 
 class IsingSimulation:
     
-    def __init__(self, n_rois, beta, coupling_mat = False, J=None, initial_state = False, state=None):
-        self.N = n_rois
+    def __init__(self, N, beta, J=1, initial_state = False, state=None):
+        self.N = N
         self.beta = beta
-        if not coupling_mat:
-            J = np.random.uniform(0, 1, size=(n_rois, n_rois))
-            J = (J + J.T)/2 # making it symmetric
-            np.fill_diagonal(J, 1)
         self.J = J
         if not initial_state:
-            self.state = 2*np.random.randint(2, size=(n_rois))-1
+            self.state = 2*np.random.randint(2, size=(N, N))-1
         else:
             self.state = state
         self.flips = {
@@ -25,61 +21,45 @@ class IsingSimulation:
         return
 
 
-    def step(self, update_state = True, state = None):
-        if update_state:
-            state = self.state[:]
-        energy_flips = 0
-        random_flips = 0
-        for i in range(self.N):
-            # calculating delH
-            H_i = 0
-            H_i = self.J[i, :] @ state.T 
-            H_i -= self.J[i, i] * state[i] # removing self coupling term
-            cost =  2 * state[i] * H_i
-            if cost < 0:
-                state[i] *= -1
-                energy_flips +=1
-            elif rand() < np.exp(-cost*self.beta):
-                state[i] *= -1
-                random_flips += 1
-        if update_state:
-            self.state = state
-        self.flips['r'].append(random_flips)
-        self.flips['e'].append(energy_flips)
-        return state
+    def step(self):
+        config = self.state[:]
+        N = self.N
+        for i in range(N):
+            for j in range(N):
+                a = np.random.randint(0, N)
+                b = np.random.randint(0, N)
+                s =  config[a, b]
+                nb = config[(a+1)%N,b] + config[a,(b+1)%N] + config[(a-1)%N,b] + config[a,(b-1)%N]
+                cost = 2*s*nb
+                if cost < 0:
+                    s *= -1
+                elif rand() < np.exp(-cost*self.beta):
+                    s *= -1
+                config[a, b] = s
+        self.state = config
+        return config
     
     def calcEnergy(self):
-        H = 0
-        H = -self.state @ self.J @ self.state.T
-        return H/2
+        config = self.state[:]
+        N = self.N
+        energy = 0 
+        for i in range(len(config)):
+            for j in range(len(config)):
+                S = config[i,j]
+                nb = config[(i+1)%N, j] + config[i,(j+1)%N] + config[(i-1)%N, j] + config[i,(j-1)%N]
+                energy += -nb*S
+        return energy/2.  # to compensate for over-counting
     
     def calcMag(self):
         mag = np.sum(self.state)
         return mag
     
     def getTimeseries(self, n_timesteps):
-        time_series = np.zeros((n_timesteps, self.N))
+        time_series = np.zeros((n_timesteps, self.N*self.N))
         state = self.state[:]
         for i in range(n_timesteps):
-            state = self.step(False, state)
-            time_series[i] = state
-        print(np.sum(time_series))
+            state = self.step()
+            time_series[i] = state.flatten()
+#         print(np.sum(time_series))
         fc = 1/n_timesteps * time_series.T @ time_series 
         return time_series, fc
-    
-    def run(self, steps, n_timesteps, calc_corr=False, fc=None, eqSteps=1000):
-        E = M = corr = []
-        for i in range(eqSteps):
-            sim.step()
-            
-        for i in range(steps):
-            if calc_corr and i%calc_corr == 0:
-                time_series, sim_fc = sim.getTimeseries(n_timesteps)
-                c = np.corrcoef(np.triu(fc).flatten(), np.triu(sim_fc).flatten())[0, 1]
-                corr.append(c)
-            sim.step()           # Monte Carlo moves
-            E.append(sim.calcEnergy())
-            M.append(sim.calcMag())
-        if calc_corr:
-            return E, M, corr
-        return E, M
