bd80b4307875bd620634a00e0858397cd3416520
Shivansh Seth <shivansh.seth@research.iiit.ac.in>
2022-08-26 15:51:00 +0530

    classifier added

---
+++ src/scripts/jp2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -n 20
+#SBATCH --gres=gpu:2
+#SBATCH --mem-per-cpu=2048
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=shivansh.seth@research.iiit.ac.in
+
+~/miniconda3/envs/brain/bin/python -u classifier.py > cgd_classifier.py
+
